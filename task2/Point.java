4f322088735b68ac537f4fe9c7e5ebc08ab27377
seriymag <84573796+seriymag@users.noreply.github.com>
2021-09-20 00:29:50 +0300

    seriymag

---
+++ task2/Point.java
@@ -0,0 +1,49 @@
+public class Point {
+
+    double x;
+     double y;
+     int location;
+
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+
+    public  String  getInfo() {
+        String x = Double.toString(getX());
+        String y = Double.toString(getY());
+        String location = Double.toString(getLocation());
+        return  x + " " + y + " " + location;
+
+    }
+
+    @Override
+    public String toString() {
+        return getInfo();
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public int getLocation() {
+        return location;
+    }
+
+    public void setLocation(int location) {
+        this.location = location;
+    }
+}
---
+++ task2/task2.java
@@ -0,0 +1,49 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class task2 {
+        private static double x;
+        private static double y;
+        private static double r;
+
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader fileReader = new BufferedReader(new InputStreamReader(new FileInputStream(reader.readLine())));
+
+        while (fileReader.ready()) {
+            String xy = fileReader.readLine();
+            String[] xyArray = xy.split(" ");
+            x = Double.parseDouble(xyArray[0]);
+            y = Double.parseDouble(xyArray[1]);
+            r = Double.parseDouble(fileReader.readLine());
+        }
+
+        BufferedReader reader1 = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader fileReader1 = new BufferedReader(new InputStreamReader(new FileInputStream(reader1.readLine())));
+
+        List<Point> points = new ArrayList<>();
+
+        while (fileReader1.ready()) {
+            String xy = fileReader1.readLine();
+            String[] xyArray = xy.split(" ");
+            points.add(new Point(Double.parseDouble(xyArray[0]), Double.parseDouble(xyArray[1])));
+        }
+
+        for (Point p : points) {
+            double value = ((p.getX() - x) * (p.getX() - x) + (p.getY() - y) * (p.getY() - y));
+            int location = value < r * r ? 1 : 2;
+            if (value == r * r) {
+                location = 0;
+            }
+            p.setLocation(location);
+            System.out.println(p.getLocation());
+        }
+
+    }
+}
